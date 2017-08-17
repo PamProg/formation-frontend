db918c9d31811fbfa128510200076cd2a282cf07
Pierre-Antoine MARTIN <pierre.antoine.martin.pro@gmail.com>
2017-08-17 17:19:54 +0200

    refactored the switch

--- formation-frontend/02-nodejs/console.js
+++ formation-frontend/02-nodejs/console.js
@@ -8,48 +8,44 @@ console.log("*** Application Conférence ***\n",
             "3. Liste des sessions\n",
             "4. Détail d'une session\n");
 
+
+var tableauOptions = [afficherNomPrenomPresentateurs, 
+                      afficherNomPrenomTopPresentateurs,
+                      afficherTitreSessions,
+                      afficherDescriptionPresentateurSession];
+
 var interface = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout,
 });
 
 interface.on("line", (line) => {
-    switch(line) {
-        case "1":
-            var presentateurs = service.listerTousLesPresentateurs();
-            afficherNomPrenomPresentateurs(presentateurs);
-            break;
-        case "2":
-            var topPresentateurs = service.listerTopPresentateurs();
-            afficherNomPrenomPresentateurs(topPresentateurs);
-            break;
-        case "3":
-            var sessions = service.listerToutesLesSessions();
-            afficherTitreSessions(sessions);
-            break;
-        case "4":
-            interface.question("Choisissez un id de session ", (idSession) => {
-                afficherDescriptionPresentateurSession(idSession);
-            });
-            break;
-        default:
-            console.log("Ca n'est pas un choix possible");
-            break;
-    }
+    tableauOptions[parseInt(line)-1]();
 });
 
-function afficherNomPrenomPresentateurs(presentateurs) {
+function afficherNomPrenomPresentateurs() {
+    var presentateurs = service.listerTousLesPresentateurs();
     presentateurs.map(p => p.firstname + " " + p.lastname)
                  .forEach(p => {console.log(p);});
 }
 
-function afficherTitreSessions(sessions) {
+function afficherNomPrenomTopPresentateurs() {
+    var topPresentateurs = service.listerTopPresentateurs();
+    topPresentateurs.map(p => p.firstname + " " + p.lastname)
+                 .forEach(p => {console.log(p);});
+}
+
+function afficherTitreSessions() {
+    var sessions = service.listerToutesLesSessions();
     sessions.map(s => s.title)
             .forEach(p => {console.log(p);});
 }
 
-function afficherDescriptionPresentateurSession(idSession) {
-    var session = service.trouverUneSession(idSession);
+function afficherDescriptionPresentateurSession() {
+    interface.question("Choisissez un id de session ", (idSession) => {
+        var session = service.trouverUneSession(idSession);
 
-    console.log("Description :", session.desc, "\nPrésentateur :", session.speakers);
+    console.log("Description :", session.desc, 
+                "\nPrésentateur :", session.speakers);
+    });
 }
