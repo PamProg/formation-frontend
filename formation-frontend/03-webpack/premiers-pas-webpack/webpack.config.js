07dc2891764440728ce191a9d3753ba8cde8ff24
Pierre-Antoine MARTIN <pierre.antoine.martin.pro@gmail.com>
2017-08-23 10:41:37 +0200

    ajout tp angular

--- formation-frontend/03-webpack/premiers-pas-webpack/webpack.config.js
+++ formation-frontend/03-webpack/premiers-pas-webpack/webpack.config.js
@@ -4,15 +4,12 @@ let HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
-    // définition des points d'entrée
-    // il est possible de définir plusieurs points d'entrée
-    entry: './app/index.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, "build"),
         filename: "bundle.js"
     },
     plugins: [
-        new UglifyJSPlugin(),
-        new HtmlWebpackPlugin()
+
     ]
 }
